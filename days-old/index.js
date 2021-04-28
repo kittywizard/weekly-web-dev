581e9206ca2913d24b7ac6dcbf16622c85285e6d
marleigh <volcana@gmail.com>
2021-04-28 10:03:06 -0700

    completing the weekly challenge. done!

--- days-old/index.js
+++ days-old/index.js
@@ -3,6 +3,7 @@ const dob = document.getElementById('dob');
 const form = document.getElementById('form');
 const results = document.querySelector('.results');
 const resultBtn = document.getElementById('btn-results');
+const container = document.querySelector('.container');
 
 btn.addEventListener('click', calcDays);
 resultBtn.addEventListener('click', () => location.reload());
@@ -11,25 +12,29 @@ function calcDays() {
     let birthday = new Date(dob.value);
     if(birthday == "Invalid Date") {
         alert('Please submit a valid date.');
+        return;
     }  else {
-        form.classList.toggle('hide');
+        container.removeChild(form); //delete the form so the new stuff centers!
     }
     
     //convert date to something JS can use
     birthday = Math.floor(birthday);  // ??? MAGIC 
     
-    //grab today's date
-    const today = Date.now(); //in milliseconds
-    
     //convert result to days
-    const daysOld = Math.floor((today - birthday) / 1000 / 60 / 60 / 24);
+    let daysOld = Math.floor((Date.now() - birthday) / 1000 / 60 / 60 / 24);
+    let daysStr = daysOld.toLocaleString();
  
     //display the answer
-    
-    //then make it look nice
-    const div = document.createElement('div');
-    div.textContent = `You are ${daysOld} days old.`;
-    results.style.display = "block";
-    results.appendChild(div);
+         //then make it look nice
+    let div = document.createElement('div');
+    let num = document.createElement('span');
+    num.textContent = `${daysStr}`;
+    num.classList.add('bold');
+    div.textContent = `You are `;
+    div.appendChild(num);
+    if(daysOld >= 12312){ div.textContent += ` days old! ☠️`}
+    else {  div.textContent += ` days old! 👶`}
+    results.style.display = 'block'
+    results.insertBefore(div, resultBtn);
     
 }

c69de516b00ebcff806d5d661e68da60762ab56b
marleigh <volcana@gmail.com>
2021-08-28 11:48:04 -0700

    basic functionality, notes for next steps

--- magic-8ball/script.js
+++ magic-8ball/script.js
@@ -1,5 +1,8 @@
 console.log('test');
 
+const advBtn = document.getElementById('adviceBtn');
+const advDisplay = document.getElementById('adviceDisplay');
+
 const advice = [
     "If you’re good at something, never do it for free.",
     "Do, or do not. There is no try.",
@@ -8,3 +11,13 @@ const advice = [
     "Life does not stop and start at your convenience.",
     "You know what the trouble about real life is? There's no danger music."
 ];
+
+advBtn.addEventListener('click', () => {
+    advDisplay.textContent = advice[0];
+});
+
+//write function to:
+    // randomly generate a number that is advice.length
+    // display it in the p
+    // generate random color background
+    // animation?

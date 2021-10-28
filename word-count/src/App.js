ba11ea893cfef0c901bebc941c1ec414874b13aa
marleigh <volcana@gmail.com>
2021-10-28 10:06:03 -0700

    adding word count component + basic styling

--- word-count/src/App.js
+++ word-count/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import TextBox from './components/TextBox';
+import WordCount from './components/WordCount';
 
 function App() {
   return (
     <div className="App">
       <TextBox />
+      <WordCount />
     </div>
   );
 }
---
+++ word-count/src/components/WordCount.js
@@ -0,0 +1,10 @@
+function WordCount() {
+    return (
+        <div className="wordcount">
+            <b>Word Count:</b> 
+            <span className="num"></span>
+        </div>
+    )
+}
+
+export default WordCount;

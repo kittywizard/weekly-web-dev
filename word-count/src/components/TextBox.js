c65360d9b13856b76883cd04ebd0ff0920467170
marleigh <volcana@gmail.com>
2021-10-28 11:58:20 -0700

    breaking things

--- word-count/src/components/TextBox.js
+++ word-count/src/components/TextBox.js
@@ -8,4 +8,16 @@ function TextBox(){
     )
 }
 
+const wordCount = document.getElementById('wordCount');
+console.log(wordCount)
+//wordCount.addEventListener('keypress', findWordCount);
+
+function findWordCount() {
+    //check each keypress for a space - return \.
+    // preceded by either a letter, period, quotation, parathensis.. etc?
+        //run further check on anything non-letter 
+    
+    console.log('key pressed')
+}
+
 export default TextBox;

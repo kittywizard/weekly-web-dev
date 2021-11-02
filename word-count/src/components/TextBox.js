4e9bab5c05e55a8e35183033e763e465b09f8227
marleigh <volcana@gmail.com>
2021-11-02 16:47:10 -0700

    it works! dif way to handle event listeners in R

--- word-count/src/components/TextBox.js
+++ word-count/src/components/TextBox.js
@@ -2,21 +2,19 @@
 function TextBox(){ 
     return (
         <div>
-            <textarea id="wordCount" name="textbox" className="textarea">
+            <textarea id="wordCount" name="textbox" className="textarea" onKeyPress={findWordCount}>
             </textarea>        
         </div>
     )
 }
-const wordCount = document.getElementById('wordCount');
-
-wordCount.addEventListener('keydown', findWordCount);
 
 function findWordCount(key) {
     //check each keypress for a space - return \.
     // preceded by either a letter, period, quotation, parathensis.. etc?
         //run further check on anything non-letter 
     
-    console.log(key.code)
+    console.log("testing" + key.code)
+    
     
 }
 
